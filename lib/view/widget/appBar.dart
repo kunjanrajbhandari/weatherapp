61c76f9e4e98baefdaf3a25c2c63e133cf3cf3ea
kunjan rajbhandari <kunjanrajbhandari@gmail.com>
2022-11-10 11:11:05 +0545

    update files

--- lib/view/widget/appBar.dart
+++ lib/view/widget/appBar.dart
@@ -29,135 +29,130 @@ class CAppBar extends StatelessWidget implements PreferredSizeWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image(
-                          image: AssetImage("assets/icons/logo.png"),
-                          height: 44,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image(
+                        image: AssetImage("assets/icons/logo.png"),
+                        height: 44,
+                      ),
+                      Container(
+                        height: 33,
+                        width: MediaQuery.of(context).size.width / 2,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 0.5),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "Enter your location...",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            Icon(Icons.search)
+                          ],
                         ),
-                        Container(
-                          height: 33,
-                          width: MediaQuery.of(context).size.width / 2,
-                          decoration: BoxDecoration(
-                              border: Border.all(width: 0.5),
-                              borderRadius: BorderRadius.circular(12)),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                "Enter your location...",
-                                style: TextStyle(color: Colors.grey),
-                              ),
-                              Icon(Icons.search)
-                            ],
-                          ),
-                        ),
-                        Icon(Icons.notifications)
-                      ],
-                    ),
+                      ),
+                      Icon(Icons.notifications)
+                    ],
                   ),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        SizedBox(
-                          height: 77,
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          child: ListView.builder(
-                              itemCount: values!.length,
-                              shrinkWrap: true,
-                              scrollDirection: Axis.vertical,
-                              itemBuilder: (context, index) {
-                                return Column(
-                                  children: [
-                                    Row(
-                                      children: [
-                                        const Icon(
-                                          Icons.location_pin,
-                                          color: Colors.black,
-                                        ),
-                                        Padding(
-                                          padding:
-                                              const EdgeInsets.only(left: 11.0),
-                                          child: RichText(
-                                              text: TextSpan(
-                                                  text:
-                                                      '${values![index].cityName}, ',
-                                                  style: const TextStyle(
-                                                      color: Colors.black,
-                                                      fontWeight:
-                                                          FontWeight.normal,
-                                                      fontSize: 17.0),
-                                                  children: [
-                                                TextSpan(
-                                                  text:
-                                                      '${values![index].countryName}',
-                                                  style: TextStyle(
-                                                      color: Colors.black
-                                                          .withOpacity(0.5),
-                                                      fontWeight:
-                                                          FontWeight.w400,
-                                                      fontSize: 15.0),
-                                                ),
-                                              ])),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                );
-                              }),
-                        ),
-                        Container(
-                          // color: Colors.red,
-                          height: 77,
-                          width: MediaQuery.of(context).size.width / 4,
-                          child: ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: pollution!.length,
-                              scrollDirection: Axis.vertical,
-                              itemBuilder: (context, index) {
-                                return Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Column(
-                                      children: [
-                                        Text("AQI"),
-                                        Text(
-                                            "${pollution![index].airQualityIndex}"),
-                                      ],
-                                    ),
-                                    Container(
-                                      decoration: BoxDecoration(
-                                          color: AqiIndexLevel().getColorLevel(
-                                              pollution![index]
-                                                  .airQualityIndex!
-                                                  .toInt()),
-                                          // shape: BoxShape.circle,
-                                          borderRadius:
-                                              BorderRadius.circular(22)),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(
-                                          AqiIndexLevel().getIndexLevel(
-                                              pollution![index]
-                                                  .airQualityIndex!
-                                                  .toInt()),
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.w500),
-                                        ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        // height: 77,
+                        width: MediaQuery.of(context).size.width / 2.2,
+                        child: ListView.builder(
+                            itemCount: values!.length,
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.location_pin,
+                                        color: Colors.black,
                                       ),
-                                    )
-                                  ],
-                                );
-                              }),
-                        )
-                      ],
-                    ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 11.0),
+                                        child: RichText(
+                                            text: TextSpan(
+                                                text:
+                                                    '${values![index].cityName}, ',
+                                                style: const TextStyle(
+                                                    color: Colors.black,
+                                                    fontWeight:
+                                                        FontWeight.normal,
+                                                    fontSize: 17.0),
+                                                children: [
+                                              TextSpan(
+                                                text:
+                                                    '${values![index].countryName}',
+                                                style: TextStyle(
+                                                    color: Colors.black
+                                                        .withOpacity(0.5),
+                                                    fontWeight: FontWeight.w400,
+                                                    fontSize: 15.0),
+                                              ),
+                                            ])),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              );
+                            }),
+                      ),
+                      Container(
+                        // color: Colors.red,
+                        height: 77,
+                        width: MediaQuery.of(context).size.width / 4,
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: pollution!.length,
+                            scrollDirection: Axis.vertical,
+                            itemBuilder: (context, index) {
+                              return Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Text("AQI"),
+                                      Text(
+                                          "${pollution![index].airQualityIndex}"),
+                                    ],
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        color: AqiIndexLevel().getColorLevel(
+                                            pollution![index]
+                                                .airQualityIndex!
+                                                .toInt()),
+                                        // shape: BoxShape.circle,
+                                        borderRadius:
+                                            BorderRadius.circular(22)),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        AqiIndexLevel().getIndexLevel(
+                                            pollution![index]
+                                                .airQualityIndex!
+                                                .toInt()),
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              );
+                            }),
+                      )
+                    ],
                   ),
                 ],
               ),
